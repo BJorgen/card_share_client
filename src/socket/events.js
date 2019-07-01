5e42a4ad69f496f2217b98d4b9ec5f37811823f3
venetrius <gergelygjuhasz@gmail.com>
2019-06-30 19:57:58 -0600

    accept update_to_card_shared and update App state regarding to it

--- src/socket/events.js
+++ src/socket/events.js
@@ -64,7 +64,7 @@ function eventHandlers(App) {
       attendee.wants = msg.wants;
       App.setState({attendee : attendee})
     },
-    
+
     categories : function(msg){
       // App.sendAlert(msg);
       msg=JSON.parse(msg);
@@ -79,7 +79,7 @@ function eventHandlers(App) {
         return;
       }
       const {requester_id, responder_id, status} = notification
-      const otherAttendeeId = getOtherAttendeeId(requester_id, responder_id, App.state.attendee.id) 
+      const otherAttendeeId = getOtherAttendeeId(requester_id, responder_id, App.state.attendee.id)
       const attendees = App.state.attendees;
       if(attendees[otherAttendeeId]){
         attendees[otherAttendeeId].connection = {sender : requester_id, status : status};
@@ -94,6 +94,24 @@ function eventHandlers(App) {
       if(attendees[id]){
         attendees[id].photo = photo;
         attendees[id].first_name = first_name;
+      }
+      App.setState({attendees});
+    },
+
+    update_to_card_shared : function(msg){
+      const notification = JSON.parse(msg);
+      const {id, photo, first_name, last_name, email_address, phone_number, position, company} = notification;
+      const attendees = App.state.attendees;
+      if(attendees[id]){
+        const target = attendees[id];
+        target.first_name = first_name;
+        target.last_name = last_name;
+        target.photo = photo;
+        target.email_address = email_address;
+        target.phone_number = phone_number;
+        target.position = position;
+        target.company = company;
+        target['linkedin-link'] = notification['linkedin-link'];
       }
       App.setState({attendees});
     },
